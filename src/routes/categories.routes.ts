1c51ed636a8a9134ce016050794d91bdb1516064
RodrigoRVSN <rodrigovictor81@gmail.com>
2021-11-08 22:48:35 -0300

    📜 swagger doc + streaming

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,26 @@
 import { Router } from 'express';
+import multer from 'multer';
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/new', (req, res) => {
-  const { name, description } = req.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  createCategoryService.execute({ name, description });
-
-  return res.status(201).send();
+  return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get('/list', (req, res) => {
-  const all = categoriesRepository.list();
+  return listCategoriesController.handle(req, res);
+});
 
-  return res.json(all);
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => {
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import swaggerConfig from '../swagger.json';
+import { router } from './routes';
 
 const PORT = process.env.PORT || 3333;
 
 const app = express();
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
+
+app.use(router);
 
 app.listen(PORT, () =>
   console.log(`🔥 Server running in http://localhost:${PORT} `),
